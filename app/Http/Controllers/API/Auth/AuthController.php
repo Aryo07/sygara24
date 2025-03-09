651b2fdf2611c22c9d104c1899bc970699c4b1a5
Aryo Kesuma <61276825+Aryo07@users.noreply.github.com>
2025-03-09 08:44:21 +0700

    add password reset  and update auth routes

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -43,9 +45,11 @@ class AuthController extends Controller
             $token = $user->createToken('authToken')->plainTextToken;
             // Mengembalikan response berikut ini
             return response()->json([
-                'success' => true,
+                'status' => 'success',
                 'message' => 'Login berhasil!',
-                'data' => $user,
+                'data' => [
+                    'user' => $user
+                ],
                 'token_type' => 'Bearer',
                 'token' => $token
             ], 200);
@@ -53,14 +57,14 @@ class AuthController extends Controller
             // Cek email jika tidak ditemukan di database maka akan mengembalikan response berikut ini
             if (!$user) {
                 return response()->json([
-                    'success' => false,
+                    'status' => 'error',
                     'message' => 'Email tidak terdaftar!',
                 ], 400);
             } else {
                 // Cek password jika tidak sesuai dengan yang ada di database maka akan mengembalikan response berikut ini
                 if (!Hash::check($request->password, $user->password)) {
                     return response()->json([
-                        'success' => false,
+                        'status' => 'error',
                         'message' => 'Password salah!',
                     ], 400);
                 }
@@ -71,12 +75,14 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         // Validasi inputan yang diterima dari request
-        $validator = Validator::make($request->all(), [
+        $validator = Validator::make(
+            $request->all(),
+            [
                 'name' => 'required',
                 'email' => 'required|email|unique:users,email',
                 'phone' => 'required|numeric|unique:users,phone',
                 'password' => 'required|min:8|confirmed'
-        ],
+            ],
             [
                 'name.required' => 'Nama tidak boleh kosong!',
                 'email.required' => 'Email tidak boleh kosong!',
@@ -111,16 +117,18 @@ class AuthController extends Controller
         // Mengembalikan response berikut ini
         if ($user) {
             return response()->json([
-                'success' => true,
+                'status' => 'success',
                 'message' => 'Register berhasil!',
-                'data' => $user,
+                'data' => [
+                    'user' => $user
+                ],
                 'token_type' => 'Bearer',
                 'token' => $token
             ], 200);
         } else {
             // Jika gagal register akan mengembalikan response berikut ini
             return response()->json([
-                'success' => false,
+                'status' => 'error',
                 'message' => 'Gagal register!',
             ], 400);
         }
@@ -137,5 +145,110 @@ class AuthController extends Controller
             'status' => 'success',
             'message' => 'Berhasil logout!',
         ], 200);
+    }
+
+    public function forgotPassword(Request $request)
+    {
+        // Validasi inputan yang diterima dari request
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'email' => 'required|email'
+            ],
+            // Kustomisasi pesan error jika validasi tidak sesuai
+            [
+                'email.required' => 'Email tidak boleh kosong!',
+                'email.email' => 'Email tidak valid!'
+            ]
+        );
+
+        // Check validation jika tidak sesuai maka akan mengembalikan response berikut ini
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Proses pengiriman link reset password ke email user
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        // Mengembalikan response berikut ini berdasarkan status pengiriman link reset password ke email user
+        return $status === Password::RESET_LINK_SENT
+            ? response()->json([
+                'status' => 'success',
+                'message' => 'Link reset password telah dikirim ke email!'
+            ], 200)
+            : response()->json([
+                'status' => 'error',
+                'message' => 'Gagal mengirim link reset password!'
+            ], 400);
+    }
+
+    public function resetPassword(Request $request)
+    {
+        // Url reset password yang dikirim ke email user akan mengandung query string token dan email user //
+        // Handle GET request //
+        // Jika user mengakses link reset password maka akan mengembalikan response berikut ini
+        if ($request->isMethod('get')) {
+            // Mengembalikan response berikut ini
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Silakan kirim permintaan POST dengan token, email, dan password baru untuk mereset password.',
+                // Tampilkan email dan token
+                'data' => [
+                    'email' => $request->query('email'),
+                    'token' => $request->query('token')
+                ]
+            ], 200);
+        }
+
+        // Jika user mengirimkan request POST untuk mereset password maka akan mengembalikan response berikut ini //
+        // Handle POST request //
+        // Validasi inputan yang diterima dari request
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'token' => 'required',
+                'email' => 'required|email',
+                'password' => 'required|min:8|confirmed'
+            ],
+            // Kustomisasi pesan error jika validasi tidak sesuai
+            [
+                'token.required' => 'Token tidak boleh kosong!',
+                'email.required' => 'Email tidak boleh kosong!',
+                'email.email' => 'Email tidak valid!',
+                'password.required' => 'Password tidak boleh kosong!',
+                'password.min' => 'Password minimal 8 karakter!',
+                'password.confirmed' => 'Konfirmasi password tidak sama!'
+            ]
+        );
+
+        // Check validation jika tidak sesuai maka akan mengembalikan response berikut ini
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Proses reset password user
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+            }
+        );
+
+        // Mengembalikan response berikut ini berdasarkan status reset password user
+        return $status === Password::PASSWORD_RESET
+            ? response()->json([
+                'status' => 'success',
+                'message' => 'Password berhasil direset!'
+            ], 200)
+            : response()->json([
+                'status' => 'error',
+                'message' => 'Gagal mereset password!'
+            ], 400);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,14 @@ use App\Http\Controllers\API\Auth\AuthController;
 
 // Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::middleware('guest')->prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->name('password.email');
+    Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
+    Route::get('/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
+});
+
 
 // Admin
 Route::prefix('admin')->middleware(['auth:sanctum', 'user-access:admin'])->group(function () {
@@ -25,8 +31,8 @@ Route::prefix('admin')->middleware(['auth:sanctum', 'user-access:admin'])->group
 });
 
 // User
-Route::middleware(['auth:sanctum', 'user-access:customer'])->group(function () {
-    
+Route::prefix('customer')->middleware(['auth:sanctum', 'user-access:customer'])->group(function () {
+
     // Logout route
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
